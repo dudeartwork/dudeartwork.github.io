5043d6f53229d6241007d83962a7b05942e73d71
dudeartwork <48885408+dudeartwork@users.noreply.github.com>
2019-07-17 02:48:16 +0530

    none

--- gallery.js
+++ gallery.js
@@ -1,5 +1,29 @@
 'use strict';
+import { photos } from "./photo";
+const e = React.createElement;
 
+class Gallery extends React.Component {
+    
+    render() {
+        return e(
+            <div>
+                <Gallery photos={photos} onClick={openLightbox} />
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                        <Modal onClose={closeLightbox}>
+                            <Carousel currentIndex={currentImage} views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                            />
+                        </Modal>
+                    ) : null}
+                </ModalGateway>
+            </div>
+        );
+    }
+}
 
 const domContainer = document.querySelector('#galleryId');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(Gallery), domContainer);
---
+++ photo.js
@@ -0,0 +1,54 @@
+export const photos = [
+    {
+      src: "assets/images/01.png",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "assets/images/02.png",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "assets/images/03.png",
+      width: 3,
+      height: 4
+    },
+    
+    {
+      src: "assets/images/05.png",
+      width: 1,
+      height: 1
+    },
+    {
+        src: "assets/images/04.jpg",
+        width: 4,
+        height: 3
+      },
+    {
+      src: "assets/images/06.png",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "assets/images/07.png",
+      width: 3,
+      height: 4
+    },
+    {
+      src: "assets/images/08.png",
+      width: 1,
+      height: 1
+    },
+    {
+      src: "assets/images/09.png",
+      width: 16,
+      height: 9
+    },
+    {
+        src: "assets/images/10.png",
+        width: 1,
+        height: 1
+      }
+  ];
+  
